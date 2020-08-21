266c794d172ad15e3513475459ca0292bde71160
kanonChakma <50201920+kanonChakma@users.noreply.github.com>
2020-08-21 18:05:13 +0600

    another file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  let person = {
+    name: "bro",
+    age: 23
+  };
+  var style = {
+    color: "red",
+    backgroundColor: "pink"
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>gfh  Start editing to see some magic happen!</h2>
+      <h1>Welcome to the new react world {3 + 5}</h1>
+      <h2 style={style}>what are you doing man</h2>
+      <h4>person info is {person.name}</h4>
     </div>
   );
 }
